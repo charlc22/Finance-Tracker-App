53e6f2b15577e8f9771aa5e06c222df7bb0bb155
sanada3 <charlyverno@gmail.com>
2025-02-06 19:11:00 -0500

    first version login feb 6

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,32 +1,32 @@
-// src/components/layout/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
+import './Navbar.css';
 
 const Navbar = () => {
     const { user, logout } = useAuth();
 
     return (
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-xl font-bold">
-                    FinanceTracker
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-brand">
+                    Wallet Wise
                 </Link>
-                <div>
+                <div className="navbar-links">
                     {user ? (
                         <>
-                            <span className="mr-4">Welcome, {user.name}</span>
+                            <span className="navbar-welcome">Welcome, {user.name}</span>
                             <button
                                 onClick={logout}
-                                className="bg-red-500 px-4 py-2 rounded hover:bg-red-600"
+                                className="navbar-button"
                             >
                                 Logout
                             </button>
                         </>
                     ) : (
                         <>
-                            <Link to="/login" className="mr-4">Login</Link>
-                            <Link to="/register">Register</Link>
+                            <Link to="/login" className="navbar-link">Login</Link>
+                            <Link to="/register" className="navbar-link">Register</Link>
                         </>
                     )}
                 </div>
